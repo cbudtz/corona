3454c8804fdc6f988130415cc005ef2714e3eb3e
Christian Budtz <chrbudtz@gmail.com>
2020-03-16 10:20:11 +0100

    retry

---
+++ build/precache-manifest.79fd4da8cde951b31574bcc7e2618295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b47e6987f39b5abc132c95ddea073b",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "79aa9764dff3f6c8db76",
+    "url": "/corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1517e9aab6c5e92224eb",
+    "url": "/corona/static/js/2.ecf72e23.chunk.js"
+  },
+  {
+    "revision": "f286d1a9ce6e5be678869cc892d2a512",
+    "url": "/corona/static/js/2.ecf72e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79aa9764dff3f6c8db76",
+    "url": "/corona/static/js/main.c2f244b6.chunk.js"
+  },
+  {
+    "revision": "7b11fb35ced4010fd88e",
+    "url": "/corona/static/js/runtime-main.0d2f1e07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
