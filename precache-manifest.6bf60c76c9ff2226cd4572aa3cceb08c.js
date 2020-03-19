71ffc11bcd5255dafb225b2bef7cb9ff638ae7d0
Christian Budtz <chrbudtz@gmail.com>
2020-03-19 13:40:40 +0100

    Manglende filer

---
+++ precache-manifest.6bf60c76c9ff2226cd4572aa3cceb08c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cbb746eeb752b155ef4cfca2996b49",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "75901c14a9f728fe1c4c",
+    "url": "/corona/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e37a9a8924ee2f0f9b33",
+    "url": "/corona/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "75901c14a9f728fe1c4c",
+    "url": "/corona/static/js/2.d3701467.chunk.js"
+  },
+  {
+    "revision": "f286d1a9ce6e5be678869cc892d2a512",
+    "url": "/corona/static/js/2.d3701467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37a9a8924ee2f0f9b33",
+    "url": "/corona/static/js/main.7b05e28d.chunk.js"
+  },
+  {
+    "revision": "749b860e5075433fead8",
+    "url": "/corona/static/js/runtime-main.377a868e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
