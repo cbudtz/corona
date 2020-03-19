4617831214c978baa04fd3dad0f55907f5636b8f
Christian Budtz <chrbudtz@gmail.com>
2020-03-19 13:45:04 +0100

    Clean up

---
+++ src/components/BasicGraph.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    ReferenceLine,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis
+} from "recharts";
+
+export const BasicGraph = ({data,dataKey}) => (
+    <ResponsiveContainer width="90%"  height={300}>
+        <LineChart
+            data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="row" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <ReferenceLine x="13/3" stroke="red" label="Ny grænse for testning" />
+            <Line type="monotone" dataKey={dataKey} stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+    </ResponsiveContainer>
+);
