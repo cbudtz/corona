2761ddc22d3cb9206f9e3b201585c54ab9269626
Christian Budtz <chrbudtz@gmail.com>
2020-04-22 19:12:02 +0200

    Opdateret intervaller og pyntet på udseende

---
+++ build/precache-manifest.6f12c681701858e8b86b8b409cd00e95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e662dfe688a544f98bfde4c007186716",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "e6e49e080189c90e81e8",
+    "url": "/corona/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d1db9d5cf9b3150f7a42",
+    "url": "/corona/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e6e49e080189c90e81e8",
+    "url": "/corona/static/js/2.83f67d5a.chunk.js"
+  },
+  {
+    "revision": "f286d1a9ce6e5be678869cc892d2a512",
+    "url": "/corona/static/js/2.83f67d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1db9d5cf9b3150f7a42",
+    "url": "/corona/static/js/main.6dc0dae5.chunk.js"
+  },
+  {
+    "revision": "749b860e5075433fead8",
+    "url": "/corona/static/js/runtime-main.377a868e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
