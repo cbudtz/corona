711798da2b9b1ff0d59e9598effbb9583efaadca
Christian Budtz <chrbudtz@gmail.com>
2020-05-07 20:37:32 +0200

    clean up

---
+++ precache-manifest.e479662fa7e37fac57aa090132aaa0ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58529011a741ca7abbe886b926d31433",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "ec45f3c718b401ed6873",
+    "url": "/corona/static/css/2.3afee639.chunk.css"
+  },
+  {
+    "revision": "87016daec1a11f6d790c",
+    "url": "/corona/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec45f3c718b401ed6873",
+    "url": "/corona/static/js/2.9904eaad.chunk.js"
+  },
+  {
+    "revision": "0229f2113b78559ca1daeb1dd663a5a5",
+    "url": "/corona/static/js/2.9904eaad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87016daec1a11f6d790c",
+    "url": "/corona/static/js/main.9b2fc48e.chunk.js"
+  },
+  {
+    "revision": "749b860e5075433fead8",
+    "url": "/corona/static/js/runtime-main.377a868e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
